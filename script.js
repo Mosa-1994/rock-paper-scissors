e714128f822b30f91e18e45f0d42084ccf958206
Mosa-1994 <197264025+Mosa-1994@users.noreply.github.com>
2025-02-22 14:13:25 +0100

    Made a start at the function in script.js.

---
+++ script.js
@@ -0,0 +1,10 @@
+function getComputerChoice (a, b, c) {
+    return Math.random()
+}
+
+/* 
+Creating a game of rock, paper, scissors.
+Computer randomly reacts with rock, paper or scissors.
+Asign rock, paper and scissors to numbers 0, 1 and 2.
+Player writes their choice.
+*/
