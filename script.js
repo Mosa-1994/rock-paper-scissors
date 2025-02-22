28bd8a542b859b21336a2d9242676d9299c51a75
Mosa-1994 <197264025+Mosa-1994@users.noreply.github.com>
2025-02-22 14:47:25 +0100

    Stored the return value of getComputerChoice to computerChoice.

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
-function getComputerChoice (a, b, c) {
-    return Math.random()
+/* function return random number 0, 1 or 2. */
+function getComputerChoice () {
+    return Math.floor(Math.random() * 2)
 }
+
+const computerChoice = getComputerChoice ();
+
+/* else if statement to get rock, paper, sciccors printed out. */
+
+if (getComputerChoice === 0) {
+    log("Rock");
+} else if (getComputerChoice === 1) {
+    log("Paper");
+} else {
+    log("Scissors");
+}
+
+
+console.log("I choose ${computerChoice}.");
+
+/* 
+if 0 log rock;
+if 1 log paper;
+if 2 log scissors;
 
 /* 
 Creating a game of rock, paper, scissors.
