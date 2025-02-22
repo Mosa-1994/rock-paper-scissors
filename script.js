733039419831393009be51b9e42a8d2077afaabd
Mosa-1994 <197264025+Mosa-1994@users.noreply.github.com>
2025-02-22 15:49:07 +0100

    trying to get the output right, by adjusting the let variabels.

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 /* function returns random number 0, 1 or 2. */
-function getComputerChoice () {
+const computerRandomNumber = function getComputerChoice () {
     return Math.floor(Math.random() * 3)
 }
-
-const computerRandomNumber = getComputerChoice ();
 
 console.log(computerRandomNumber);
 
@@ -23,12 +21,57 @@ console.log(`I choose ${computerChoice}.`);
 
 /* Players side of the game */
 function getHumanChoice () {
-    prompt("Make your choice.")
+    const humanInput = prompt("Make your choice.");
+    return humanInput.toLowerCase;
 }
 
 let humanChoice = getHumanChoice ();
 
 console.log(humanChoice);
+
+/* Player wins if: 
+computer  |  Player
+---------------------
+Rock      |  Paper
+Paper     |  Sciccors
+Sciccors  |  Rock
+
+Computer wins if
+Computer  |  Player
+-------------------
+Rock      |  Sciccors
+Paper     |  Rock
+Sciccors  |  Paper
+*/
+
+
+let playerWins = '';
+let computerWins = '';
+let tie = '';
+if (humanChoice === "Paper" && computerChoice === "Rock") {
+   playerWins 
+} else if (humanChoice === "Sciccors" && computerChoice === "Paper") {
+    playerWins
+} else if (humanChoice === "Rock" && computerChoice === "Sciccors") {
+    playerWins
+} else if (computerChoice === "Rock" && humanChoice === "Sciccors") {
+    computerWins
+} else if (computerChoice === "Paper" && humanChoice === "Rock") {
+    computerWins
+} else if (computerChoice === "Sciccors" && humanChoice === "Paper") {
+    tie
+} else {
+    console.log('Oops... that went wrong.')
+}
+
+
+if (playerWins) {
+    console.log(`3... 2... 1... ${computerChoice.toUpperCase}! Congrats you've won!`)
+} else if (computerWins) {
+    console.log(`3... 2... 1... ${computerChoice.toUpperCase}! Mwhuahaha I win, you lose!`)
+} else if (tie) {
+    console.log(`It's a tie... Let's try again.`)
+}
 
 /* 
 if 0 log rock;
